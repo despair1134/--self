3163b96b4091b943d688787f0a6c082ee592a5c0
luo981695830 <37054307+luo981695830@users.noreply.github.com>
2019-08-21 12:53:29 +0800

    Delete urls.py

--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from .views import user_register,user_logout,user_login
-
-urlpatterns = [
-    url(r'user_register/$',user_register,name='user_register'),
-    url(r'user_login/$',user_login,name='user_login'),
-    url(r'user_logout/$',user_logout,name='user_logout'),
-]
-
